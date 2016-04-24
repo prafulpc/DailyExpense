4d80afca2a1e2cb1fe121460e747a752e9121a75
PRAFUL <prafulpc045@gmail.com>
2016-04-24 20:02:10 +0530

    24/4/16

--- app/src/main/java/com/example/android/dailyexpense/MainActivity.java
+++ app/src/main/java/com/example/android/dailyexpense/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,13 +26,11 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("onCreate","MainActivity");
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
